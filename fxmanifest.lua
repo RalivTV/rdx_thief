14d934a5e39075fdff0f2772097d734f07448318
Nevermind <58900773+RalivTV@users.noreply.github.com>
2021-05-24 07:47:45 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,21 +3,21 @@ fx_version 'adamant'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'ESX THIEF'
+description 'RDX THIEF'
 
 version '1.0.1'
 
 client_scripts {
-    '@redm_extended/locale.lua',
+    '@rdx_core/locale.lua',
     'locales/br.lua',
     'config.lua',
     'client/main.lua',
-	'handsup.lua'
+    'handsup.lua'
 }
 
 server_scripts {
-    '@redm_extended/locale.lua',
+    '@rdx_core/locale.lua',
     'locales/br.lua',
     'config.lua',
-	'server/main.lua'
+    'server/main.lua'
 }
